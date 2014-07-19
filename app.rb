62e290fda43b8e64adfaf6e956c0899bbe39a748
Carla Souza <contact@carlasouza.com>
2014-07-18 22:26:03 -0300

    cosmetics

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ set :static, enable
 set :dump_errors, true
 set :raise_errors, false
 set :show_exceptions, false
-set :port, 80
-set :bind, '162.252.240.6'
+# set :port, 80
+# set :bind, '162.252.240.6'
 
 configure :production do
   $host = 'http://buzzcommit.com'
@@ -30,7 +30,7 @@ get '/robots.txt' do
 end
 
 get '/ta' do
-  'Copyright Carla Souza <contact@carlasouza.com>'
+  CGI.escapeHTML 'Copyright Carla Souza <contact@carlasouza.com>'
 end
 
 not_found { redirect to('/') }
@@ -43,16 +43,23 @@ __END__
 !!!
 %html
   %head
-    %title Buzzfeed style Commits
+    %title BuzzCommit - BuzzFeed-style commit messages generator
     %meta{content: "svn, github, buzzfeed, git, commit, message, coding, programming", name: "keywords"}/
-    %meta{content: "Commits Buzzfeed Style", name: "description"}/
+    %meta{content: "BuzzCommit - BuzzFeed-style commit messages generator", name: "description"}/
     %link{href: "http://buzzcommit.com/", rel: "canonical"}/
-    :css
-      * {margin: 0;} html, body {height: 100%;} .wrapper {min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -8em;} .footer, .push {height: 8em;}
+    %link{href: "http://fonts.googleapis.com/css?family=Montserrat:700", rel: "stylesheet", type: "text/css"}/
+    %link(rel="stylesheet" type="text/css" href="style.css")
   %body
     .wrapper
-      %center{style: "color: #333; padding-top: 200px; font-family: Courier; font-size: 24px; font-weight: bold;"}
-        %a{href: "/", rel: "nofollow", style: "text-decoration: none; color: #333;"}= @msg.downcase.capitalize + '.'
+      %center
+        %h1
+          What if programmers used
+          %span.red.buzzfeed BuzzFeed
+          style commit messages?
+        %p
+          git commit -m "
+          %a.buzzfeed.message{href: "/", rel: "nofollow"}= @msg.downcase.capitalize + '.'
+          "
       .push
       %center
         %iframe{allowtransparency: "true", frameborder: "0", scrolling: "no", src: "//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fbuzzcommit.com&width&layout=button&action=like&show_faces=false&share=false&height=20&appId=106768876164959", style: "border:none; overflow:hidden; height:20px; width: 70px;"}
@@ -60,7 +67,7 @@ __END__
         :javascript
           !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
     .footer
-      %center{style: "color: #333; font-family: Courier; font-size: 11px;"}
+      %center
 
         %script{async: "", src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"}
         / Buzzfeed commit msg
@@ -69,7 +76,8 @@ __END__
           (adsbygoogle = window.adsbygoogle || []).push({});
 
         %br/
-        %a{href: 'http://carlasouza.com'} made with ♥
+        %a.red{title: 'Made by', href: 'http://carlasouza.com'}♥
+        %a{title: 'Inspiration', href: 'https://storify.com/anirvan/buzzfeed-style-commit-messages'}♕
     :javascript
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-52950937-1']);
@@ -81,4 +89,4 @@ __END__
        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
       })();
-    
+
